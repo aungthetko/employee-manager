1f4d98a9c577d5592994ad925de3e264bc8b7c56
Aung Thet Ko <oungthetko18893@gmail.com>
2021-08-20 22:59:36 +0630

    first commit

---
+++ src/app/service/employee.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { Employee } from '../model/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  // find all employees
+  public getEmployees() : Observable<Employee[]>{
+    return this.http.get<Employee[]>(`${this.apiUrl}/employee/all`);
+  }
+
+  // add new employee
+  public addEmployee(employee: Employee) : Observable<Employee>{
+    return this.http.post<Employee>(`${this.apiUrl}/employee/add`, employee);
+  }
+
+  // update an employee
+  public updateEmployee(employee: Employee) : Observable<Employee>{
+    return this.http.put<Employee>(`${this.apiUrl}/employee/update/`, employee);
+  }
+  
+  // delete an employee
+  public delete(employeeId: number | undefined) : Observable<void>{
+    return this.http.delete<void>(`${this.apiUrl}/employee/delete/${employeeId}`);
+  }
+
+  // get generate employee
+  public getGenerateEmployee() : Observable<Employee[]>{
+    return this.http.get<Employee[]>(`${this.apiUrl}/employee/generate`);
+  }
+
+  public deleteAllEmployees(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/employee/delete/all`)
+  }
+}
